fcaf518684dfc0daf53bff700736b849a8557996
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-03-08 14:44:59 +0000

    Added further sample files and figured out how to see something from console

---
+++ redirect.js
@@ -0,0 +1,12 @@
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+      console.log('bob');
+      alert("Hello from your Chrome extension!");
+    }
+  }
+);
