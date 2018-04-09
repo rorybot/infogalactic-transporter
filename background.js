29a37a13e3bba9094bbc2936d19fb7f173261cc8
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-04-09 16:05:23 +0100

    Redirect no longer autoblocks manual redirect back to Wiki

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 var redirected = false
+var wikiIdentifier = 'wikipedia.org/wiki'
+var igIdentifier = 'infogalactic.com/info'
 // Called when the user clicks on the browser action.
 
 function prepForEntry(url){
@@ -10,8 +12,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     var currentURL = activeTab.url
-    var wikiIdentifier = 'wikipedia.org/wiki'
-    var igIdentifier = 'infogalactic.com/info'
 
     if(currentURL.includes(wikiIdentifier)){
       currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
@@ -19,7 +19,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
       // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": igURL});
       chrome.tabs.update({"url": igURL});
-      redirected = true
     }
 
     if(currentURL.includes(igIdentifier)){
@@ -31,40 +30,23 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 });
 
-chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   var activeTab = tabs[0];
   var currentURL = activeTab.url
-  var wikiIdentifier = 'wikipedia.org/wiki'
-  var igIdentifier = 'infogalactic.com/info'
 
 
-  chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {
-            redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
-        };
-    },
-    {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
-    ['blocking']
-  );
+    chrome.webRequest.onBeforeRequest.addListener(
+      function(details) {
+          if(redirected == false){
+                      var redirected = true;
+                      return {
+                          redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
+                      };
+          }
+
+
+      },
+      {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
+      ['blocking']
+    );
 });
-
-
-
-
-
-
-//
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if( request.message === "redirect" ) {
-//       chrome.tabs.update({"url": request.url});
-//       console.log('bob')
-//     }
-//
-//     if (request.message === "goodbye") {
-//       redirected = false
-//       console.log('the redirect status is '+redirected)
-//     }
-//   }
-// );
