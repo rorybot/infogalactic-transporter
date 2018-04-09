05430d8e0855fc46078ac49a686e0403502cf377
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-04-09 17:04:47 +0100

    Manual redirect disables autoredirect, until you leave either wikipedia or IG, then resets

--- background.js
+++ background.js
@@ -1,52 +1,62 @@
-var redirected = false
+var allowRedirect = true
 var wikiIdentifier = 'wikipedia.org/wiki'
 var igIdentifier = 'infogalactic.com/info'
-// Called when the user clicks on the browser action.
 
 function prepForEntry(url){
   return url.replace(/https:\/\/.+?\./, 'https://')
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    var currentURL = activeTab.url
+chrome.browserAction.onClicked.addListener(
+  function(tab) {
+  chrome.tabs.query(
+    {active: true, currentWindow: true},
+    function(tabs) {
+      var activeTab = tabs[0];
+      var currentURL = activeTab.url
+      allowRedirect = false
 
-    if(currentURL.includes(wikiIdentifier)){
-      currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
-      console.log(currentURL)
-      var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
-      // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": igURL});
-      chrome.tabs.update({"url": igURL});
-    }
+      if(currentURL.includes(wikiIdentifier)){
+        currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
+        var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
+        chrome.tabs.update({"url": igURL});
+      }
 
-    if(currentURL.includes(igIdentifier)){
-      var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
-      // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": wikiURL});
-      chrome.tabs.update({"url": wikiURL});
-    }
+      if(currentURL.includes(igIdentifier)){
+        var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
+        chrome.tabs.update({"url": wikiURL});
+      }
   });
 
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var activeTab = tabs[0];
-  var currentURL = activeTab.url
-
+chrome.tabs.query(
+  {active: true, currentWindow: true},
+  function(tabs) {
+    var activeTab = tabs[0];
+    var currentURL = activeTab.url
 
     chrome.webRequest.onBeforeRequest.addListener(
       function(details) {
-          if(redirected == false){
-                      var redirected = true;
-                      return {
-                          redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
-                      };
-          }
-
-
+        if(allowRedirect == true){
+          return {
+            redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
+          };
+        }
       },
       {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
       ['blocking']
     );
-});
+  }
+);
+
+
+
+chrome.tabs.onUpdated.addListener(
+  function(tabid, changeInfo, tab) {
+    if (changeInfo.status == 'complete'){
+      if(!tab.url.includes(wikiIdentifier) && !tab.url.includes(igIdentifier)){
+        allowRedirect = true
+      }
+    }
+  }
+);
