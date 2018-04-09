5119c39da57d0c6892f74d090e9da0dc7a493293
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-04-09 15:44:57 +0100

    Redirects automatically; removed dependence on messages; moved variables up in scope

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
-
+var redirected = false
 // Called when the user clicks on the browser action.
+
+function prepForEntry(url){
+  return url.replace(/https:\/\/.+?\./, 'https://')
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -12,21 +17,54 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
       console.log(currentURL)
       var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": igURL});
+      // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": igURL});
+      chrome.tabs.update({"url": igURL});
+      redirected = true
     }
 
     if(currentURL.includes(igIdentifier)){
       var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": wikiURL});
+      // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "redirectedURL": wikiURL});
+      chrome.tabs.update({"url": wikiURL});
     }
   });
+
 });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.update({"url": request.url});
-      console.log('bob')
-    }
-  }
-);
+chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+  var activeTab = tabs[0];
+  var currentURL = activeTab.url
+  var wikiIdentifier = 'wikipedia.org/wiki'
+  var igIdentifier = 'infogalactic.com/info'
+
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {
+            redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
+        };
+    },
+    {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
+    ['blocking']
+  );
+});
+
+
+
+
+
+
+//
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "redirect" ) {
+//       chrome.tabs.update({"url": request.url});
+//       console.log('bob')
+//     }
+//
+//     if (request.message === "goodbye") {
+//       redirected = false
+//       console.log('the redirect status is '+redirected)
+//     }
+//   }
+// );
--- redirect.js
+++ redirect.js
@@ -5,7 +5,15 @@ chrome.runtime.onMessage.addListener(
 
       console.log('I have the URL and it is', request.redirectedURL)
 
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": request.redirectedURL});
+      chrome.runtime.sendMessage({"message": "redirect", "url": request.redirectedURL});
     }
   }
 );
+
+
+if(window.location.origin.includes('wikipedia.org')){
+  window.onbeforeunload = function() {
+    chrome.runtime.sendMessage({"message": "goodbye"});
+    console.log("arrggghhh")
+  }
+}
