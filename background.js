9420e502dcb68d1f90d1d84aa6d19039669f8603
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-04-13 09:39:30 +0100

    Behaviour is now bound to a tab, and state held in an associative array
    - redirectedArray holds tabId and whether redirect is allowed
    - redirecredArray is updated depending on whether the button is clicked or navigation away from either encyclopedia is performed
    - Also removed an unncessary tabs query around the onBeforeRequest listener

--- background.js
+++ background.js
@@ -1,62 +1,55 @@
-var allowRedirect = true
 var wikiIdentifier = 'wikipedia.org/wiki'
 var igIdentifier = 'infogalactic.com/info'
+var redirectedArray = {}
 
-function prepForEntry(url){
+function prepForEntry (url) {
   return url.replace(/https:\/\/.+?\./, 'https://')
 }
 
 chrome.browserAction.onClicked.addListener(
-  function(tab) {
-  chrome.tabs.query(
-    {active: true, currentWindow: true},
-    function(tabs) {
-      var activeTab = tabs[0];
-      var currentURL = activeTab.url
-      allowRedirect = false
+  function (tab) {
+    chrome.tabs.query(
+      {active: true, currentWindow: true},
+      function (tabs) {
+        var activeTab = tabs[0]
+        var currentURL = activeTab.url
+        redirectedArray[activeTab.id] = 'disallowRedirect'
 
-      if(currentURL.includes(wikiIdentifier)){
-        currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
-        var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
-        chrome.tabs.update({"url": igURL});
-      }
-
-      if(currentURL.includes(igIdentifier)){
-        var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
-        chrome.tabs.update({"url": wikiURL});
-      }
-  });
-
-});
-
-chrome.tabs.query(
-  {active: true, currentWindow: true},
-  function(tabs) {
-    var activeTab = tabs[0];
-    var currentURL = activeTab.url
-
-    chrome.webRequest.onBeforeRequest.addListener(
-      function(details) {
-        if(allowRedirect == true){
-          return {
-            redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
-          };
+        if (currentURL.includes(wikiIdentifier)) {
+          currentURL = currentURL.replace(/https:\/\/.+?\./, 'https://')
+          var igURL = currentURL.replace(wikiIdentifier, igIdentifier)
+          chrome.tabs.update({'url': igURL})
         }
-      },
-      {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
-      ['blocking']
-    );
-  }
-);
 
+        if (currentURL.includes(igIdentifier)) {
+          var wikiURL = currentURL.replace(igIdentifier, wikiIdentifier)
+          chrome.tabs.update({'url': wikiURL})
+        }
+      })
+  })
 
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    if (!(details.tabId in redirectedArray)) {
+      redirectedArray[details.tabId] = 'allowRedirect'
+    }
+    if (redirectedArray[details.tabId] === 'allowRedirect') {
+      return {
+        redirectUrl: prepForEntry(details.url).replace(wikiIdentifier, igIdentifier)
+      }
+    }
+  },
+  {urls: ['*://*.wikipedia.org/*'], types: ['main_frame']},
+  ['blocking']
+)
 
 chrome.tabs.onUpdated.addListener(
-  function(tabid, changeInfo, tab) {
-    if (changeInfo.status == 'complete'){
-      if(!tab.url.includes(wikiIdentifier) && !tab.url.includes(igIdentifier)){
-        allowRedirect = true
+  function (tabid, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+      console.log(redirectedArray)
+      if (!tab.url.includes(wikiIdentifier) && !tab.url.includes(igIdentifier)) {
+        redirectedArray[tabid] = 'allowRedirect'
       }
     }
   }
-);
+)
